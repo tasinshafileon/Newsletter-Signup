800dbea374430e0048f1a6411770970e3a3656a1
Tasin Shafi Leon <leontasinshafi@gmail.com>
2021-11-03 00:24:47 +0600

    First Commit

---
+++ js/index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+const request = require('request');
+const https = require('https');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../signup.html"));
+});
+
+app.post("/", function(req, res) {
+
+  const data = {
+    members: [{
+      email_address: req.body.email,
+      status: "subscribed",
+      merge_fields: {
+        NAME: req.body.name
+      }
+    }]
+  }
+
+  const jsonData = JSON.stringify(data);
+  const mailChimpURL = "https://us5.api.mailchimp.com/3.0/lists/2d1bba181a"
+
+  const options = {
+    method: "post",
+    auth: "tasinshafileon:a313a0891a49d3162947c02373bea2d9d-us5"
+  }
+
+  const request = https.request(mailChimpURL, options, function(response) {
+    if (response.statusCode === 200) {
+      res.sendFile(path.join(__dirname, "../success.html"));
+    } else {
+      res.sendFile(path.join(__dirname, "../failure.html"));
+    }
+
+  });
+
+  request.write(jsonData);
+  request.end();
+
+});
+
+app.post("/success", function(req, res){
+  res.redirect("/");
+});
+
+app.post("/failure", function(req, res){
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Virtual server running on localhost:3000")
+});
